638ab14e5781c901a2640b5b0884abbeccfc87b3
kamol <debnath.kamolkrishna1@gmail.com>
2026-03-11 21:22:25 +0600

    fix login

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,28 @@ import { webpack } from "next/dist/compiled/webpack/webpack";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  reactStrictMode: true,
+  serverRuntimeConfig: {
+    API_URL: process.env.NEXTAPI_URL,
+  },
+  publicRuntimeConfig: {
+    PUBLIC_API_URL: process.env.NEXTAPI_URL,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'http://103.164.160.22',
+        port: '2352',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
